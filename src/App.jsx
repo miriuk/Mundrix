4c47925c1a77b4d8e14ff4340fe12649c905d759
FabricioMiriuk <miriuk@gmail.com>
2025-06-18 16:27:12 +0100

    feat: generate 3D blocks from text prompt

--- src/App.jsx
+++ src/App.jsx
@@ -187,7 +187,7 @@ console.log("Seed:", seed);
   </mesh>
 <mesh receiveShadow rotation={[-Math.PI / 2, 0, 0]} position={[0, 0, 0]}>
   <planeGeometry args={[50, 50]} />
-  <meshStandardMaterial color="#333" />
+  <meshStandardMaterial color="#fff" />
 </mesh>
   <OrbitControls />
 </Canvas>
